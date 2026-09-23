5ed289aee0b9cbde09db1cec33d26e3a76d3971c
Anders Lerche <anderslerches@gmail.com>
2026-09-23 13:24:30 +0200

    rettet

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+"use strict"
 // Skriv "use strict" herunder
 
 function beregnSum(number1, number2) {
     // Funktionen skal beregne summen af de to tal og udskrive resultatet i konsollen, fx: "Summen af 15 og 27 er 42"
-
+   let resultat = number1 + number2;
+   console.log(`Sum af ${number1} og ${number2} er ${resultat}`);
 }
 
 
 // Skriv en funktion, der hedder beregnDifference, og som tager to parametre: number1 og number2
+
 // Funktionen skal beregne forskellen mellem de to tal og udskrive resultatet i konsollen, fx: "Forskellen mellem 27 og 15 er 12"
 
 
